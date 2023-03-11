546d113e9faba49c174ae2ee94b283803cc61298
danghuydieu1243 <danghuydieu1243@gmail.com>
2023-03-11 17:23:15 +0700

    Edit recharge lan 1

--- DoAnWinDows/UCRecharge.cs
+++ DoAnWinDows/UCRecharge.cs
@@ -12,9 +12,21 @@ namespace DoAnWinDows
 {
     public partial class UCRecharge : UserControl
     {
+        AccountDao accountdao= new AccountDao();
         public UCRecharge()
         {
             InitializeComponent();
+        }
+
+        private void btnShowInfor_Click(object sender, EventArgs e)
+        {
+            Account account = new Account();
+            account.Identitycard=txtAccountNumber.Text;
+            LoadDanhSach(account);
+        }
+        private void LoadDanhSach(Account account)
+        {
+            this.gvInforAccount.DataSource = accountdao.ShowInfomation(account);
         }
     }
 }
